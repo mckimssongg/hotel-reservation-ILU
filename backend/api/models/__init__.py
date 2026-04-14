8ca511ceeace8a3781ca1fe75ce02967bc4771f4
mckimssongg <soymckimsson286@gmail.com>
2026-04-13 22:10:49 -0600

    fix

--- backend/api/models/__init__.py
+++ backend/api/models/__init__.py
@@ -1,6 +1,6 @@
 from api.models.entradaListaEspera import EntradaListaEspera
 from api.models.habitacion import Habitacion
-from api.models.modeloBase import ModeloBase
+from api.models.base import ModeloBase
 from api.models.precioNocheReserva import PrecioNocheReserva
 from api.models.reserva import Reserva
 from api.models.tipoHabitacion import TipoHabitacion
